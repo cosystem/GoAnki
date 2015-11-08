518daeddd9f7a54efd8083959d9a9ef5c0b55f35
cosystem <jougraw@gmail.com>
2015-11-08 22:59:51 +0000

    version of multithread and simplfied class Modifictions: 1. simplify the class 2. add multithread to speed up the processing 3. save output to csv 4. polished the filenames for in- and out-put files

--- google_translator.py
+++ google_translator.py
@@ -1,58 +1,84 @@
 __author__ = 'PGY'
 
-import codecs, sys
-import requests
+import sys
+import os.path
 import datetime
+import requests
+import threading
 
 class TransCrawler:
-    def __init__(self, urlnkeyword):
+    '''Web Crawler to get the translated word from google translation'''
+    def __init__(self, inlang, outlang, theword):
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         self.headers = {'User-Agent' : self.user_agent}
-        self.urlnkeyword = urlnkeyword
+        self.inlang = inlang
+        self.outlang = outlang
+        self.theword = theword
+        self.keywords ={'sl': self.inlang, 'tl': self.outlang, 'ie':'UTF-8', 'q': self.theword}
+        self.url = 'http://translate.google.com/m'
 
     def getPage(self):
+        url = self.url
+        keywords = self.keywords
         try:
-            response = requests.get(self.urlnkeyword[0], self.urlnkeyword[1], headers = self.headers)
+            response = requests.get(url, keywords, headers = self.headers)
             content = response.text
             return content
         except requests.exceptions.RequestException as e:
             print(e)
             return None
 
-    def getPageItem(self):
+    def getWord(self):
         mark='class="t0">'
         content = self.getPage()
         if not content:
             print('Load page failed!')
             return None
         else:
+            #return content
             startpos = content.index(mark)
             remaincont = content[content.find(mark)+len(mark):]
             result = remaincont.split('<')[0]
             return result
 
-    def start(self):
-        result = self.getPageItem()
-        print(result)
+def transword_writeoutput(inword, outfilename):
+    '''read a word string and save the input word and the output translation into a csv file'''
+    inlang = 'de'
+    outlang = ['en', 'zh']
+    output_list = [inword]
+    for lan in outlang:
+        newword = TransCrawler(inlang, lan, inword)
+        output_list.append(newword.getWord())
+    outstr = ",".join(output_list) + "\n"
+    with open(outfilename, 'a', encoding='utf-8') as text_file:
+        text_file.write(outstr)
 
-class LangURLInfo:
-    def __init__(self, inlang, outlang):
-        self.inlang = inlang
-        self.outlang = outlang
-        self.theword = ''
-
-    def geturl(self):
-        keywords ={'sl': self.inlang, 'tl': self.outlang, 'ie':'UTF-8', 'q': self.theword}
-        url = 'http://translate.google.com/m'
-        return (url, keywords)
-
+# get the input word list
+inputfile = sys.argv[1]
 datemark = datetime.datetime.now().strftime('%m.%Y')
-with open(sys.argv[1], 'r', encoding = 'utf-8') as f:
+with open(inputfile, 'r', encoding = 'utf-8') as f:
     first_line = f.readline()
-
 inwordlist = first_line.split(datemark)[1].split()
-info=LangURLInfo('de','en')
-for word in inwordlist:
-    info.theword = word
-    crawler = TransCrawler(info.geturl())
-    crawler.start()
+unique_inwordlist = list(set(inwordlist))
+
+# define output file name
+outfilename = os.path.splitext(inputfile)[0] + '_GoAnki.csv'
+
+# if the output already exists in current direcotry, remove it. Otherwise do nothing.
+try:
+    os.remove(outfilename)
+except OSError:
+    pass
+
+# translate the words in word list and save the results in a csv file with multithread (speed up)
+jobs = []
+for word in unique_inwordlist:
+    thread = threading.Thread(target=transword_writeoutput, args = (word,), kwargs = {'outfilename' : outfilename})
+    jobs.append(thread)
+
+for j in jobs:
+    j.start()
+
+for j in jobs:
+    j.join()
+
