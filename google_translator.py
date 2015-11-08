97d5d6576859e3996ccde8b3fbf5c9c5bf3e78b4
cosystem <jougraw@gmail.com>
2015-11-08 10:47:17 +0000

    process multi-entries

--- google_translator.py
+++ google_translator.py
@@ -1,6 +1,8 @@
 __author__ = 'PGY'
 
+import codecs, sys
 import requests
+import datetime
 
 class TransCrawler:
     def __init__(self, urlnkeyword):
@@ -37,14 +39,20 @@ class LangURLInfo:
     def __init__(self, inlang, outlang):
         self.inlang = inlang
         self.outlang = outlang
-        self.theword = input('Please enter a German word: ')
+        self.theword = ''
 
     def geturl(self):
         keywords ={'sl': self.inlang, 'tl': self.outlang, 'ie':'UTF-8', 'q': self.theword}
         url = 'http://translate.google.com/m'
         return (url, keywords)
 
+datemark = datetime.datetime.now().strftime('%m.%Y')
+with open(sys.argv[1], 'r', encoding = 'utf-8') as f:
+    first_line = f.readline()
 
+inwordlist = first_line.split(datemark)[1].split()
 info=LangURLInfo('de','en')
-crawler = TransCrawler(info.geturl())
-crawler.start()
+for word in inwordlist:
+    info.theword = word
+    crawler = TransCrawler(info.geturl())
+    crawler.start()
